4fb12f62de6a443ac238a64724170c4f3241bec1
uchiwaSeek <uchiwa0905@gmail.com>
2020-10-21 01:44:07 +0900

    add new file

---
+++ sample.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+//using System.Diagnostics;
+using UnityEngine;
+
+  
+public class sample : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        string s;
+        s = "うちわ";
+        int n = 10;
+        float z = 3.2f;
+        bool b = true;
+        bool bb = false;
+        Debug.Log(s);
+        Debug.Log(n); 
+        Debug.Log(b);
+        Debug.Log(z);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
